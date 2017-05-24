3702744d0fc6f194630297ad0725a59fb7ab09bc
tischulz1 <tischulz@cebitec.uni-bielefeld.de>
2017-05-25 01:50:01 +0200

    Add files via upload

---
+++ scripts/MakeHomList.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+
+mfile = open(sys.argv[1], "r")
+
+homList = []
+
+for line in mfile:
+	cols = line.split(',')
+
+	homList.append((cols[0], cols[3]))
+
+mfile.close()
+
+hfile = open(sys.argv[2], "r")
+
+humanGeneDict = {}
+
+for line in hfile:
+	cols = line.split(',')
+
+	if not cols[2] in humanGeneDict:
+		humanGeneDict[cols[2]] = cols[3]
+
+hfile.close()
+
+hFile = open(sys.argv[3], "w")
+
+for ent in homList:
+	hFile.write(humanGeneDict[ent[0]] + '\t' + ent[1]+ "\n")
