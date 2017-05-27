3418e571a0d392774f2e790d6866a6645f101de7
tischulz1 <tischulz@cebitec.uni-bielefeld.de>
2017-05-27 16:11:12 +0200

    Small adjustments

--- scripts/MakeHomList.py
+++ scripts/MakeHomList.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
 
 import sys
-import csv
 
 mfile = open(sys.argv[1], "r")
-homList = [(cols[0], cols[3]) for cols in csv.reader(mfile, delimiter='\t')]
+
+homList = []
+
+for line in mfile:
+	cols = line.split(',')
+
+	homList.append((cols[0], cols[3]))
+
 mfile.close()
 
 hfile = open(sys.argv[2], "r")
+
 humanGeneDict = {}
 
-for cols in csv.reader(hfile, delimiter='\t'):
-    if cols[0] not in humanGeneDict:
-        humanGeneDict[cols[0]] = cols[3]
+for line in hfile:
+	cols = line.split(',')
+
+	if not cols[2] in humanGeneDict:
+		humanGeneDict[cols[2]] = cols[3]
 
 hfile.close()
 
 hFile = open(sys.argv[3], "w")
-for ent1, ent2 in homList:
-    hFile.write(humanGeneDict[ent1] + '\t' + ent2+ "\n")
+
+for ent in homList:
+	hFile.write(humanGeneDict[ent[0]] + '\t' + ent[1]+ "\n")
