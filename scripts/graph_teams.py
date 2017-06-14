bfe5cd7dfb87d9d997a4fdb565e822603c166c26
Daniel Doerr <daniel.doerr@epfl.ch>
2017-06-14 09:58:19 +0200

    critical bugfix and minor improvements

--- scripts/graph_teams.py
+++ scripts/graph_teams.py
@@ -16,13 +16,15 @@ if not hasattr(nx.Graph, 'nodes_iter'):
     setattr(nx.Graph, 'nodes_iter', nx.Graph.nodes)
 
 
-def next_delta_connected(G, S, delta):
+def next_delta_connected(G, S, delta, source=None):
     """ traverses graph G by BFS: in each step, the function reports a
     (previously unreported) vertex s of S as long as there exists a path with
     distance <= delta from an already reported member to to s; requires that
     source must be part of S"""
 
-    source = choice(S)
+    if source == None:
+        source = choice(S)
+        
     S = set(S)
     yield source
     visited = set((source, ))
@@ -42,11 +44,11 @@ def next_delta_connected(G, S, delta):
         queue.extend((v, w, dv) for w in G.neighbors(v))
 
 
-def smallMax(Ss, Gs, delta):
+def smallMax(Ss, Gs, delta, source=None):
     """ identify smallest \delta-set in either G or H """
    
-    BFS_handles = [next_delta_connected(Gs[i], Ss[i], delta) for i in
-            xrange(len(Gs))]
+    BFS_handles = [next_delta_connected(Gs[i], Ss[i], delta, source and
+        source[i] or None) for i in xrange(len(Gs))]
 
     Vs = [set() for _ in xrange(len(Gs))]
     ps = [True for _ in xrange(len(Gs))]
@@ -63,7 +65,7 @@ def smallMax(Ss, Gs, delta):
 
     # search for any maximal \delta-set that is smaller than its input set...
     for i in xrange(len(Gs)-1):
-        if len(Vs[i]) != len(Ss[i]):
+        if not ps[i] and len(Vs[i]) != len(Ss[i]):
             return i, Vs[i] 
     #... if none is found, return first set by default
     return len(Gs)-1, Vs[len(Gs)-1]
@@ -112,39 +114,43 @@ def findDeltaTeams(Gs, delta):
         Ss = [list(chain(*NG[i].values())) for i in xrange(len(Gs))]
         i, Sp = smallMax(Ss, Gs, delta)
         if len(Ss[i]) == len(Sp):
-            if any(imap(lambda x: len(x) > 1, Ss)):
+            if all(imap(lambda x: len(x) > 1, Ss)):
                 res.append(NG)
         else:
             queue.append(NG)
-            queue.append(division(i, Sp, set(Gs[i].node[v]['class'] for v in
-                Sp), NG))
+            queue.append(division(i, Sp, Gs[i], NG))
     return res
 
 
-def division(i, S, C, NG):
+def division(i, S, Gi, NG):
     """ parititions each table in NG, into two separate tables, overlapping
     only at vertices with shared classes. """
 
     NGX = [dict() for _ in NG]
+
+    C = set()
+
+    for v in S:
+        c = Gi.node[v]['class']
+        NG[i][c].remove(v)
+        if not NGX[i].has_key(c):
+            NGX[i][c] = set()
+        NGX[i][c].add(v)
+        C.add(c)
+
     # split class table
     for c in C:
-        # first, split the table NG[i] (where V is from)
-        NGX[i][c] = set()
-        for v in list(NG[i][c]):
-            if v in S:
-                NG[i][c].remove(v)
-                NGX[i][c].add(v)
-        if not len(NG[i][c]):
-            del NG[i][c]
-       
         # copy/move entries of NGX[j] table 
         for j in xrange(len(NG)):
             if j == i:
-                continue
-            if NG[i].has_key(c):
-                NGX[j][c] = set(NG[j][c])
+                # remove the entry NG[i] (where S is from) if empty
+                if not NG[i][c]:
+                    del NG[i][c]
             else:
-                NGX[j][c] = NG[j].pop(c)
+                if NG[i].has_key(c) and NG[i][c]:
+                    NGX[j][c] = set(NG[j][c])
+                else:
+                    NGX[j][c] = NG[j].pop(c)
     return NGX
 
 
