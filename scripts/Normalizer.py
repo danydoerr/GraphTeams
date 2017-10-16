8fbe207265628e23d1cca275af43d7c87dbaf251
tischulz1 <tischulz@cebitec.uni-bielefeld.de>
2017-10-16 09:24:49 +0200

    Generalization to handle inter-chromosomal maps
    The normalization and graph building procedures are now able to process inter-chromosomal Hi-C maps.

--- scripts/Normalizer.py
+++ scripts/Normalizer.py
@@ -5,7 +5,8 @@ import numpy as np
 import argparse as args
 
 parser = args.ArgumentParser(description="Creates distance matrices out of Hi-C maps.")
-parser.add_argument('M', metavar='Hi-C_Map', type=str, nargs='+', help="Path of all Hi-C maps to consider for the normalization")
+parser.add_argument('M', metavar='Hi-C_Map', type=str, nargs='+', help="Path of all Hi-C maps to consider for the "
+                                                                       + "normalization")
 
 argus = parser.parse_args()
 
@@ -13,45 +14,46 @@ mapList = [np.genfromtxt(i, dtype=str) for i in argus.M]
 
 print("Loading done")
 
-#compute the average of all maximum entries of all matrices
+# compute the average of all maximum entries of all matrices
 maxInMats = []
 
 for mAp in mapList:
-	entries = []
-	diagEntrs = []
+    entries = []
+    diagEntrs = []
 
-	for i in range(len(mAp)):
-		for j in range(len(mAp[i])):
-			if mAp[i][j] != "NULL":
-				entries.append(float(mAp[i][j]))
-				if j == i + 1:
-					diagEntrs.append(float(mAp[i][j]))
+    for i in range(len(mAp)):
+        for j in range(len(mAp[i])):
+            if mAp[i][j] != "NULL":
+                entries.append(float(mAp[i][j]))
+                if j == i + 1:
+                    diagEntrs.append(float(mAp[i][j]))
 
-	matMax = max(entries)
+    matMax = max(entries)
 
-	#Compute the average value on the diagonal above the main diagonal of every chromosome
-	avgAbDiag = 0.0
+    # Compute the average value on the diagonal above the main diagonal of every chromosome
+    avgAbDiag = 0.0
 
-	for entr in diagEntrs:
-		avgAbDiag += entr
+    for entr in diagEntrs:
+        avgAbDiag += entr
 
-	avgAbDiag = avgAbDiag / float(len(diagEntrs))
+    avgAbDiag /= float(len(diagEntrs))
 
-	maxInMats.append(matMax)
+    maxInMats.append(matMax)
 
-	#change entries to distances
-	for i in range(len(mAp)):
-		for j in range(len(mAp[i])):
-			if mAp[i][j] != "NULL":
-				mAp[i][j] = str(matMax + 1.0 - float(mAp[i][j]))
-			if i == j:
-				mAp[i][j] = str(matMax + 1.0 - avgAbDiag)
+    # change entries to distances
+    for i in range(len(mAp)):
+        for j in range(len(mAp[i])):
+            if mAp[i][j] != "NULL":
+                mAp[i][j] = str(matMax + 1.0 - float(mAp[i][j]))
+            # for the main diagonal of interchromosomal maps we do not need to insert artificial distances
+            if i == j and len(mAp) == len(mAp[i]):
+                mAp[i][j] = str(matMax + 1.0 - avgAbDiag)
 
-#Calculate the average maximum over all chromosomes
+# Calculate the average maximum over all chromosomes
 maxSum = 0.0
 
 for maxi in maxInMats:
-	maxSum += maxi
+    maxSum += maxi
 
 avgMax = maxSum / float(len(maxInMats))
 
@@ -59,33 +61,34 @@ overallMax = max(maxInMats)
 
 print("Begin normalization")
 
-#Normalize each map
+# Normalize each map
 for i in range(len(mapList)):
-	#get normalization constant NOTE: Taking the average here leads to smaller distances in the matrices. I personally prefer this
-	c = avgMax / maxInMats[i]
+    # get normalization constant NOTE: Taking the average here leads to smaller distances in the matrices. I personally
+    # prefer this
+    c = avgMax / maxInMats[i]
 
-	#Normalization
-	for k in range(len(mapList[i])):
-		for l in range(len(mapList[i][k])):
-			#we do not consider null entries
-			if mapList[i][k][l] != "NULL":
-				#store new value in map
-				mapList[i][k][l] = str(c * float(mapList[i][k][l]))
+    # Normalization
+    for k in range(len(mapList[i])):
+        for l in range(len(mapList[i][k])):
+            # we do not consider null entries
+            if mapList[i][k][l] != "NULL":
+                # store new value in map
+                mapList[i][k][l] = str(c * float(mapList[i][k][l]))
 
 print("Normalisation done")
 
-#saving the matrices
+# saving the matrices
 for i in range(len(mapList)):
-	mapfile = open(argus.M[i] + ".dmat", "w")
+    mapfile = open(argus.M[i] + ".dmat", "w")
 
-	for k in range(len(mapList[i])):
-		for l in range(len(mapList[i][0])):
-			mapfile.write(mapList[i][k][l])
+    for k in range(len(mapList[i])):
+        for l in range(len(mapList[i][0])):
+            mapfile.write(mapList[i][k][l])
 
-			#check if we have read the end of the line
-			if l + 1 != len(mapList[i][0]):
-				mapfile.write("\t")
-		#check if we have reached the last line
-		if k + 1 != len(mapList[i]):
-			mapfile.write("\n")
-	mapfile.close()
+            # check if we have read the end of the line
+            if l + 1 != len(mapList[i][0]):
+                mapfile.write("\t")
+        # check if we have reached the last line
+        if k + 1 != len(mapList[i]):
+            mapfile.write("\n")
+    mapfile.close()
--- scripts/ParseToGraphml.py
+++ scripts/ParseToGraphml.py
@@ -4,104 +4,119 @@ import sys
 import argparse as args
 
 #Function to create a graphml element that represents a weighted edge between two nodes
-def cEdge(iD, orig, dest, w):
-    return "    edge [\n        source " + str(orig) + "\n        target " + str(dest) + "\n        weight " + str(w) + "\n    ]\n"
+def cEdge(orig, dest, w, d):
+    if not d:
+        return "    edge [\n        source " + str(orig) + "\n        target " + str(dest) + "\n        weight " + \
+               str(w) + "\n    ]\n"
+    elif w <= d:
+        return "    edge [\n        source " + str(orig) + "\n        target " + str(dest) + "\n        weight " + \
+               str(w) + "\n    ]\n"
+    else:
+        return ""
 
 #Setting up the argument parser
 
 parser = args.ArgumentParser(description="Creates a file in graphml format for the gene teams algorithm")
-parser.add_argument('A', metavar='AnnotationFile', type=str, help="Path to the file that contains all gene annotations. Should be in gff3 format")
-parser.add_argument('H', metavar='HomologyTable', type=str, help="Path to the file that contains names of homologous genes of a gene family. Should be a tab separated file in which each line is a gene family and each column contains the names of homologous genes of a species.")
-parser.add_argument('T', metavar='TargetFile', type=str, help="Path to the file in which the graph sould be written to.")
-parser.add_argument('M', metavar='Hi-C_Map', type=str, nargs='+', help="Path to the file that contains a Hi-C map. Should be a tab separated file consisting of three colums containing the pair of bins and their count.")
+parser.add_argument('A', metavar='AnnotationFile', type=str, help="Path to the file that contains all gene annotations."
+                                                                  + "Should be in gff3 format")
+parser.add_argument('H', metavar='HomologyTable', type=str, help="Path to the file that contains names of homologous ge"
+                                                                 + "nes of a gene family. Should be a tab separated fil"
+                                                                 + "e in which each line is a gene family and each colu"
+                                                                 + "mn contains the names of homologous genes of a spec"
+                                                                 + "ies.")
+parser.add_argument('T', metavar='TargetFile', type=str, help="Path to the file in which the graph sould be written to."
+                    )
+parser.add_argument('M', metavar='Hi-C_Map', type=str, nargs='+', help="Path to the file that contains a Hi-C map. Shou"
+                                                                       + "ld be a tab separated file consisting of thre"
+                                                                       + "e colums containing the pair of bins and thei"
+                                                                       + "r count.")
 parser.add_argument('-s', '--binSize', default=0, type=int, help="Size of bin of the Hi-C map, i.e. its resolution.")
+parser.add_argument('-d', '--delta', default=None, type=int, help="Ignore matrix values larger than DELTA to build the "
+                                                                  + "Graph.")
 
 arguments = parser.parse_args()
 
-#Check if bin size is set
+# Check if bin size is set
 if arguments.binSize <= 0:
     print >> sys.stderr, "Bin sizes not or not correctly set."
     exit(-1)
 
-#Write the header
+# Write the header
 oFile = open(arguments.T, "w")
 oFile.write("graph [\n    directed 0\n")
 
-#Reading in homology table
+# Reading in homology table
 homologies = {}
 
 hFile = open(arguments.H, "r")
 
-#For each gene family...
+# For each gene family...
 for fam in hFile:
-    #get rid of the newline character
+    # get rid of the newline character
     fam = fam.replace("\n", "")
 
-    #split the line by column
+    # split the line by column
     homs = fam.split("\t")
 
-    #store the gene family in a dictionary
+    # store the gene family in a dictionary
     homologies[homs[0]] = homs
 
 hFile.close()
 
-#Reading in the annotation file
+# Reading in the annotation file
 geneBins = {}
 
-#Counter for unique node and edge ids
+# Counter for unique node and edge ids
 nC = 0
 eC = 0
 
 aFile = open(arguments.A, "r")
 
-#For each line in the annotation file...
+# For each line in the annotation file...
 for ann in aFile:
     ann = ann.replace("\n", "")
 
-    #split the line by its columns
+    # split the line by its columns
     infos = ann.split("\t")
 
     fName = infos[3]
 
     name = None
 
-    #check if the gene is homologous
+    # check if the gene is homologous
     for fam in homologies.values():
         if fName in fam:
             name = fam[0]
 
-            #create node for gene
-            oFile.write("    node [\n        id " + str(nC) + "\n        label " + str(nC) + '\n        class "' + name + '"\n    ]\n')
+            # create node for gene
+            oFile.write("    node [\n        id " + str(nC) + "\n        label " + str(nC) + '\n        class "' + name
+                        + '"\n    ]\n')
 
-            #find out which bin corresponds to the current gene
+            # find out which bin corresponds to the current gene
             binNum = str((int(infos[1]) / int(arguments.binSize)) + 1)
 
-            #calculate middle of the gene 
+            # calculate middle of the gene
             mid = int(infos[1]) + ((int(infos[2]) - int(infos[1])) / 2)
 
-            #save to which bin the gene corresponds
+            # save to which bin the gene corresponds
             if binNum in geneBins:
                 geneBins[binNum].append((infos[0], nC, mid))
             else:
                 geneBins[binNum] = [(infos[0], nC, mid)]
 
-            #increment node counter
+            # increment node counter
             nC += 1
 
 aFile.close()
 
-#Read in Hi-C map
+# Read in Hi-C map
 maps = arguments.M
 
+'''
 #Dictionary to store amount of contacts for each region
 contCounts = {}
 for i in range(len(maps)):
     mFile = open(maps[i], "r")
-
-    #find out which chromosome we are dealing with
-    mapName = maps[i].split('/')[-1]
-
-    chrom = mapName.split('chr')[1].split('.')[0]
 
     #a counter for save the number of lines already read in (because this is the bin number)
     l = 0
@@ -119,11 +134,18 @@ for i in range(len(maps)):
         #each line has a tab at the end therefore we need to delete it first
         cell.pop()
 
-        #first chromosome the current map belongs to
-        fChr = chrom
+        # find out which chromosome we are dealing with
+        mapName = maps[i].split('/')[-1]
 
-        #second chromosome the current map belongs to
-        sChr = chrom
+        # first chromosome the current map belongs to
+        fChr = mapName.split('chr')[1].split('.')[0]
+
+        # check whether we are dealing with a intrachromosomal map
+        if len(mFile) == len(pair):
+            # second chromosome the current map belongs to
+            sChr = fChr
+        else:
+            sChr = mapName.split('chr')[2].split('.')[0]
  
         #sum up all entries of this line
         overallContacts = 0
@@ -165,17 +187,12 @@ for i in range(len(maps)):
             contCounts[curChr] = {str(l): overallContacts}
 
     mFile.close()
-
-#Add edges to the graph
+'''
+# Add edges to the graph
 for i in range(len(arguments.M)):
-    mFile = open(arguments.M[i], "r")
+    mFile = open(arguments.M[i], "r").readlines()
 
-    #find out which chromosome we are dealing with
-    mapName = maps[i].split('/')[-1]
-
-    chrom = mapName.split('chr')[1].split('.')[0]
-
-    #counter for lines that are already read in to determine the current bin
+    # counter for lines that are already read in to determine the current bin
     lCounter = 0
 
     for line in mFile:
@@ -185,53 +202,57 @@ for i in range(len(arguments.M)):
 
         cols = line.split('\t')
 
+        # find out which chromosomes we are dealing with
+        mapName = maps[i].split('/')[-1]
+
+        fchrom = mapName.split('chr')[1].split('.')[0]
+        if len(mFile) != len(cols):
+            schrom = mapName.split('chr')[2].split('.')[0]
+        else:
+            schrom = fchrom
+
         for j in range(len(cols)):
-            #we first consider the entries on the main diagonal
-            if lCounter  - 1 == j and str(j + 1) in geneBins:
+            # we first consider the entries on the main diagonal (if we are dealing with an intrachromosomal map)
+            if len(mFile) == len(cols) and lCounter - 1 == j and str(j + 1) in geneBins:
                 genes = geneBins[str(j + 1)]
 
                 for k in range(len(genes)):
-                    if genes[k][0] == chrom:
+                    if genes[k][0] == fchrom:
                         for l in range(len(genes)):
-                            if k < l and genes[l][0] == chrom:
+                            if k < l and genes[l][0] == schrom:
 
                                 #calculate distance of 1bp
                                 dbp = float(cols[j]) / float(arguments.binSize)
                                 w = dbp * (abs(genes[k][2] - genes[l][2]))
 
-                                oFile.write(cEdge(eC, genes[k][1], genes[l][1], w))
+                                oFile.write(cEdge(genes[k][1], genes[l][1], w, arguments.delta))
 
-                                eC += 1
-            #now we only want to consider all entries above the main diagonal since  
-            #entries below the main diagonal are redundant
-            if lCounter - 1 < j and str(j + 1) in geneBins:
+                                # eC += 1
+            # now we only want to consider all entries above and on (if we are not dealing with an intrachromosomal map)
+            # the main diagonal since entries below the main diagonal are redundant
+            elif lCounter - 1 <= j and str(j + 1) in geneBins:
                 for gene in geneBins[str(j + 1)]:
-                    if gene[0] == chrom:
+                    if gene[0] == fchrom:
                         if str(lCounter) in geneBins:
                             for ogene in geneBins[str(lCounter)]:
-                                if ogene[0] == chrom and j < len(cols) and cols[j] != "NULL" and float(cols[j]) > 0:
+                                if ogene[0] == schrom and j < len(cols) and cols[j] != "NULL" and float(cols[j]) > 0:
 
                                     w = float(cols[j])
 
-                                    oFile.write(cEdge(eC, gene[1], ogene[1], w))
-
-                                    eC += 1
-
-                                if ogene[0] == chrom and j < len(cols) and cols[j] == "NULL" and lCounter == j:
+                                    oFile.write(cEdge(gene[1], ogene[1], w, arguments.delta))
+                                if ogene[0] == schrom and j < len(cols) and cols[j] == "NULL" and lCounter == j and \
+                                        len(mFile) == len(cols):
                                     dbp = float(cols[j-1]) / float(arguments.binSize)
                                     w = dbp * (abs(gene[2] - ogene[2]))
 
-                                    oFile.write(cEdge(eC, gene[1], ogene[1], w))
-
-                                    eC += 1
-                                if ogene[0] == chrom and j < len(cols) and cols[j] != "NULL" and float(cols[j]) == 0 and lCounter == j:
+                                    oFile.write(cEdge(gene[1], ogene[1], w, arguments.delta))
+                                if ogene[0] == schrom and j < len(cols) and cols[j] != "NULL" and float(cols[j]) == 0 \
+                                        and lCounter == j and len(mFile) == len(cols):
                                     dbp = float(cols[j-1]) / float(arguments.binSize)
                                     w = dbp * (abs(gene[2] - ogene[2]))
 
-                                    oFile.write(cEdge(eC, gene[1], ogene[1], w))
+                                    oFile.write(cEdge(gene[1], ogene[1], w, arguments.delta))
 
-                                    eC += 1
-
-#Finish the output file
+# Finish the output file
 oFile.write("]")
 oFile.close()
